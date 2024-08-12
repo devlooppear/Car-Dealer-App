ecb378ff39dc3f2210ae93f099cb57c69961ff83
devlooppear <iago.profissional.developer@gmail.com>
2024-08-12 18:57:06 -0300

    feat: footer

--- src/app/common/footer.tsx
+++ src/app/common/footer.tsx
@@ -1,9 +1,14 @@
 const Footer = () => {
-    return (
-        <footer>
-            <p>Footer Content</p>
-        </footer>
-    );
+  return (
+    <div className="flex flex-col justify-center align-middle items-center">
+      <div className="bg-neutral-400 min-h-[3vh] w-full text-center text-neutral-50">
+        <h2 className="font-semibold my-1">Car Dealer App</h2>
+      </div>
+      <footer>
+        <p>Footer Content</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
